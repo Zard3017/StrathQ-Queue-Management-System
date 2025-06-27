bf93a20ba4258a80ec2d3847b0c88292039620b8
Bradley Ian Lusalenge <ianbradley3017@gmail.com>
2025-06-27 09:55:58 +0300

    added functionality for creating user in sign up

--- strathq_project/myapp/views.py
+++ strathq_project/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.shortcuts import authenticate
+
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login
 
 # Create your views here.
 def login(request):
@@ -21,9 +23,36 @@ def login(request):
             return redirect('login')
     else:
         return render(request, 'login.html')
-    
+
    
 def signup(request):
+    if request.method == 'POST':
+        # Get the user data from the form
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        #Validation
+        if not username or not email or not password:
+            messages.error(request, 'All fields are required.')
+            return redirect('signup')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists.')
+            return redirect('signup')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Email already exists.')
+            return redirect('signup')
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            messages.success(request, 'Account created successfully.')
+            return redirect('login')
+        except Exception as e:
+            messages.error(request, 'An error occurred while creating the account.')
+            return redirect('signup')
+
+        
     return render(request, 'signup.html')
 
 def student(request):
